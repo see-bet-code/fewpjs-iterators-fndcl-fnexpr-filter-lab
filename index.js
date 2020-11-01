e4b8981de875a38d214244737250f2957c2f34f2
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-01 12:53:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter( driver => {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( driver => {
+        return driver.startsWith(string)
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter( driver => {
+        return driver.name === string
+    })
+}
+
